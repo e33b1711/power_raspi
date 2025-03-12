dbba94c223d698e8fd5d3492622c86edf2c49aa4
AK <e33b1711@gmail.com>
2025-03-12 13:01:00 +0100

    Fix git get

--- lib/git_revision.py
+++ lib/git_revision.py
@@ -1,6 +1,7 @@
 """Get git # + dirty flag"""
-from subprocess import run, PIPE
 import logging
+import git
+
 
 # logging stuff
 
@@ -10,13 +11,11 @@ logger.setLevel("INFO")
 
 def get_git_rev():
     """Return git version string"""
-    ret = run("git rev-parse HEAD", stdout=PIPE, text=True)
-    build_version = ret.stdout.strip()
-    ret = run("git status --porcelain", stdout=PIPE, text=True)
-    dirty = not ret.stdout.strip() == ""
-    if dirty:
-        revision = "dirty_" + build_version[0:7]
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+    if repo.is_dirty():
+        rev_string = "dirty_" + sha[0:7]
     else:
-        revision = build_version[0:7]
-    logger.info("Git revision: %s", revision)
-    return revision
+        rev_string = sha[0:7]
+    logger.info("Git rev string: %s", rev_string)
+    return rev_string
